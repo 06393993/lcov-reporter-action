7a5192dc45135c9922b4c2b54efa76038afa2896
Kaiyi Li <kaiyili@google.com>
2023-07-28 20:04:03 -0700

    Allow post the report to job summary

--- dist/main.js
+++ dist/main.js
@@ -110983,6 +110983,7 @@ async function main() {
 		core.getInput("filter-changed-files").toLowerCase() === "true";
 	const shouldDeleteOldComments =
 		core.getInput("delete-old-comments").toLowerCase() === "true";
+	const postTo = core.getInput("post-to").toLowerCase();
 	const title = core.getInput("title");
 
 	const raw = await require$$0$1.promises.readFile(lcovFile, "utf-8").catch(err => null);
@@ -111029,20 +111030,32 @@ async function main() {
 		await deleteOldComments(githubClient, options, context);
 	}
 
-	if (context.eventName === "pull_request" || context.eventName === "pull_request_target") {
-		await githubClient.issues.createComment({
-			repo: context.repo.repo,
-			owner: context.repo.owner,
-			issue_number: context.payload.pull_request.number,
-			body: body,
-		});
-	} else if (context.eventName === "push") {
-		await githubClient.repos.createCommitComment({
-			repo: context.repo.repo,
-			owner: context.repo.owner,
-			commit_sha: options.commit,
-			body: body,
-		});
+	switch (postTo) {
+		case "comment":
+			if (
+				context.eventName === "pull_request" ||
+				context.eventName === "pull_request_target"
+			) {
+				await githubClient.issues.createComment({
+					repo: context.repo.repo,
+					owner: context.repo.owner,
+					issue_number: context.payload.pull_request.number,
+					body: body,
+				});
+			} else if (context.eventName === "push") {
+				await githubClient.repos.createCommitComment({
+					repo: context.repo.repo,
+					owner: context.repo.owner,
+					commit_sha: options.commit,
+					body: body,
+				});
+			}
+			break
+		case "job-summary":
+			await core.summary.addRaw(body).write();
+			break
+		default:
+			core.warning(`Unknown post-to value: '${postTo}'`);
 	}
 }
 
